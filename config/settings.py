897ed897f1ff1fde8b22400c5703a79f0cdb585a
Saugat Sg <saugat987.sg@gmail.com>
2026-05-20 22:56:31 +0545

    Forth Test

--- config/settings.py
+++ config/settings.py
@@ -169,9 +169,9 @@ STATICFILES_STORAGE = (
 # =========================
 
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.getenv('CLOUD_NAME'),
-    'API_KEY': os.getenv('API_KEY'),
-    'API_SECRET': os.getenv('API_SECRET'),
+    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
 }
 
 DEFAULT_FILE_STORAGE = (
